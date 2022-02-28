58fb5284e4798193df102623580555ba3d619d03
Ketu-Patel <ketupatel1010@gmail.com>
2022-02-27 23:01:32 -0500

    Added Workout Box component
    Added Workout Box component using ng create component and added html and css to make it look nice.

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { WorkoutBoxComponent } from './workout-box/workout-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    WorkoutBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ Iron-Therapy/src/app/workout-box/workout-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-box',
+  templateUrl: './workout-box.component.html',
+  styleUrls: ['./workout-box.component.css']
+})
+export class WorkoutBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
