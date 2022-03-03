80977d82585dbbcd87c406721887826ec4a2ce48
James Liu <liuxjames88@gmail.com>
2022-03-02 23:52:09 -0500

    Revert "create analysis component and a testing component"
    This reverts commit 31fe24b072cfeb6231f6c513776a8c70fd0437ff.

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AnalysisComponent } from './analysis/analysis.component';
-import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    AnalysisComponent,
-    DummyComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
