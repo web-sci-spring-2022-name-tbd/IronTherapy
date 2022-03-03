b3ca5092cc8080477c3bb9fe5f9b77e4c07f0ff9
James Liu <liuxjames88@gmail.com>
2022-03-02 23:54:21 -0500

    reverting a revert pt 1

--- Iron-Therapy/src/app/nav/nav.component.ts
+++ Iron-Therapy/src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  @Output() changeNavEvent = new EventEmitter<string>();
+  
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  emitScore(page: string) {
+    this.changeNavEvent.next(page);
   }
 
 }
