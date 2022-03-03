31fe24b072cfeb6231f6c513776a8c70fd0437ff
James Liu <liuxjames88@gmail.com>
2022-03-02 22:49:55 -0500

    create analysis component and a testing component

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    AnalysisComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
