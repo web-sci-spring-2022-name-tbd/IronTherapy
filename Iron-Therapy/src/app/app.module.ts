647b1f77bf76033c98343e6f9699e497d4602564
BriLo0916 <41803333+BriLo0916@users.noreply.github.com>
2022-03-02 23:25:48 -0500

    created exercise-box component

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { ExerciseBoxComponent } from './exercise-box/exercise-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ExerciseBoxComponent
   ],
   imports: [
     BrowserModule,
