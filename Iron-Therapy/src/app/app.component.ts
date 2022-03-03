8d23f0ad7fa3413eb37d141fbe8f604e1653a794
James Liu <liuxjames88@gmail.com>
2022-03-02 23:51:17 -0500

    clicking on icons now changes pages

--- Iron-Therapy/src/app/app.component.ts
+++ Iron-Therapy/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DummyComponent } from './dummy/dummy.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Iron-Therapy';
+  pages = ['analysis', 'dummy'];
+
+  ngOnInit(): void {
+    this.changePage('dummy');
+  }
+
+  // hide all of the other pages
+  changePage(page: string) {
+    console.log(`changing page to ${page}`)
+    this.pages.forEach(page_ => {
+      if (page_ !== page) {
+        console.log(`hiding ${page_}`)
+        let el = document.getElementById(page_) as HTMLElement;
+        hide(el);
+      }
+    });
+
+    // show the specific page
+    let el = document.getElementById(page) as HTMLElement;
+    show(el);
+  }
 }
+
+
+// Show an element
+function show (elem: HTMLElement) {
+	elem.style.display = 'block';
+};
+
+// Hide an element
+function hide (elem: HTMLElement) {
+	elem.style.display = 'none';
+};
