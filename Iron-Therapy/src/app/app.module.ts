345ca235e2f68a978cb401a4f1f80c35c48a1b93
RussellABH <75578919+RussellABH@users.noreply.github.com>
2022-03-03 20:31:44 -0500

    Setup homepage
    Made component and installed bootstrap

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
