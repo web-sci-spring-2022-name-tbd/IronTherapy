e565a4381f923b69e0ad06a5a5f203a936e39db8
Carter Ellis <carter.d.ellis10@gmail.com>
2022-03-03 23:35:31 -0500

    Login HTML and CSS

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
