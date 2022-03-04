85a5e0aab293930156a71fa7bf149863ad27e44c
Ketu-Patel <ketupatel1010@gmail.com>
2022-03-03 23:02:46 -0500

    Done with History Box and workout box
    Done with History Box and workout box

--- Iron-Therapy/src/app/history-box/history-box.component.ts
+++ Iron-Therapy/src/app/history-box/history-box.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryBoxComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
+
 
   ngOnInit(): void {
   }
