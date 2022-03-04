1cc3853f3d6c28babe82d6290b80742a2ea14d74
James Liu <liuxjames88@gmail.com>
2022-03-03 22:01:52 -0500

    Revert "creating navbar component"
    This reverts commit 95515b676346b340c4cea5c207db95684d936060.

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
