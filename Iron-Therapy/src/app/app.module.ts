f38871db9ed15ef6d67b9c29210442db3556bdf0
RussellABH <75578919+RussellABH@users.noreply.github.com>
2022-03-04 01:20:39 -0500

    Initial homepage finished

--- Iron-Therapy/src/app/app.module.ts
+++ Iron-Therapy/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { GoalComponent } from './goal/goal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomepageComponent
+    HomepageComponent,
+    GoalComponent
   ],
   imports: [
     BrowserModule,
