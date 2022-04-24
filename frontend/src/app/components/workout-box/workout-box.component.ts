abfef7441afddc02a12b11136e6b68787a3383af
Ketu-Patel <ketupatel1010@gmail.com>
2022-04-24 17:34:26 -0400

    Updating workout and history box from previous branch

--- frontend/src/app/components/workout-box/workout-box.component.ts
+++ frontend/src/app/components/workout-box/workout-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-workout-box',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-box.component.css']
 })
 export class WorkoutBoxComponent implements OnInit {
-
+  @Input() photo: String;
+  @Input() workout_name: String;
   constructor() { }
 
   ngOnInit(): void {
