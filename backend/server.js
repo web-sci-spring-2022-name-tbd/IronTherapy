ee54d98d216437025f526ccd147f72f613d1245f
Carter Ellis <carter.d.ellis10@gmail.com>
2022-04-25 12:48:05 -0400

    Added rate limiting

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const goalRoutes = require('./routes/goal-routes');
 const exerciseRoutes = require('./routes/exercise-routes');
 const cors = require('cors');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 const decode = require('./decode');
 
@@ -23,6 +24,13 @@ database.once("connected", () => {
   console.log("Database Connected");
 });
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 
 app.use(cors());
@@ -31,9 +39,9 @@ app.use(express.static(path.join(__dirname, "../frontend/dist/iron-therapy")));
 
 app.use(bodyParser.json());
 
-app.use('/workouts', decode.decodeToken, workoutRoutes);
-app.use("/goals", decode.decodeToken, goalRoutes);
-app.use("/exercises", decode.decodeToken, exerciseRoutes);
+app.use('/workouts', limiter, decode.decodeToken, workoutRoutes);
+app.use("/goals", limiter, decode.decodeToken, goalRoutes);
+app.use("/exercises", limiter, decode.decodeToken, exerciseRoutes);
 
 // This must be the last get statement, dont put any app.use below it
 app.get("*", (req, res) => {
