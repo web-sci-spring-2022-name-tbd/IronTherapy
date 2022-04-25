0172b3368baabee0fdfe5bf81128b8f72743fc08
RussellABH <75578919+RussellABH@users.noreply.github.com>
2022-04-25 17:56:20 -0400

    Only return an array of strings

--- backend/routes/exercise-routes.js
+++ backend/routes/exercise-routes.js
@@ -6,8 +6,13 @@ router.get("/", async (req, res) => {
   try {
     // Need to put in firebase auth stuff
     const data = await Exercise.find({});
+    let toReturn = [];
+    // Loop through all exercises and add to toReturn
+    for (let i = 0; i < data.length; i++) {
+      toReturn.push(data[i].name);
+    }
     console.log("Got all exercises");
-    res.status(200).json(data);
+    res.status(200).json(toReturn);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
