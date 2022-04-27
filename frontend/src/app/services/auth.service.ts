fd1a3a9440596294db1a5573e8bf0e026e77ac4c
Carter Ellis <carter.d.ellis10@gmail.com>
2022-04-27 19:08:56 -0400

    Fixed double login

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,4 @@
-import { RequestsService } from 'src/app/services/requests.service';
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
 import * as auth from 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -8,7 +7,6 @@ import {
   AngularFirestoreDocument,
 } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -18,9 +16,7 @@ export class AuthService {
   constructor(
     public afs: AngularFirestore, // Inject Firestore service
     public afAuth: AngularFireAuth, // Inject Firebase auth service
-    public router: Router,
-    public ngZone: NgZone, // NgZone service to remove outside scope warning
-    private http: HttpClient
+    public router: Router
   ) {
     /* Saving user data in localstorage when
     logged in and setting up null when logged out */
@@ -28,96 +24,109 @@ export class AuthService {
       if (user) {
         this.userData = user;
         localStorage.setItem('user', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user')!);
       } else {
         localStorage.setItem('user', 'null');
-        JSON.parse(localStorage.getItem('user')!);
       }
     });
   }
+
   async reloadUser() {
-    const user = await this.afAuth.currentUser;
-    await user?.reload();
-    localStorage.setItem('user', JSON.stringify(this.userData));
-    JSON.parse(localStorage.getItem('user')!);
+    try {
+      const user = await this.afAuth.currentUser;
+      await user!.reload();
+      localStorage.setItem('user', JSON.stringify(this.userData));
+      JSON.parse(localStorage.getItem('user')!);
+    } catch (e) {
+      console.error(e);
+    }
   }
+
   async getToken(): Promise<string | undefined> {
-    const user = await this.afAuth.currentUser;
-    return await user?.getIdToken(true);
+    let token: string | undefined;
+    try {
+      const user = await this.afAuth.currentUser;
+      token = await user?.getIdToken(true);
+    } catch (e) {
+      console.error(e);
+    }
+    return token;
   }
+
   // Sign in with email/password
-  SignIn(email: string, password: string) {
-    return this.afAuth
-      .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
-        this.SetUserData(result.user);
-      })
-      .catch((error) => {
-        window.alert(error.message);
-      });
+  async SignIn(email: string, password: string) {
+    try {
+      const result = await this.afAuth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+
+      localStorage.setItem('user', JSON.stringify(result.user));
+      this.SetUserData(result.user);
+      this.router.navigate(['dashboard']);
+    } catch (e) {
+      console.error(e);
+    }
   }
+
   // Sign up with email/password
-  SignUp(email: string, password: string) {
-    return this.afAuth
-      .createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        /* Call the SendVerificaitonMail() function when new user sign
-        up and returns promise */
-        this.SendVerificationMail();
-        this.SetUserData(result.user);
-      })
-      .catch((error) => {
-        window.alert(error.message);
-      });
+  async SignUp(email: string, password: string) {
+    try {
+      const result = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      this.SendVerificationMail();
+      this.SetUserData(result.user);
+    } catch (e) {
+      console.error(e);
+    }
   }
+
   // Send email verfificaiton when new user sign up
-  SendVerificationMail() {
-    return this.afAuth.currentUser
-      .then((u: any) => u.sendEmailVerification())
-      .then(() => {
-        this.router.navigate(['verify-email']);
-      });
+  async SendVerificationMail() {
+    try {
+      const user = await this.afAuth.currentUser;
+      await user!.sendEmailVerification();
+      this.router.navigate(['verify-email']);
+    } catch (e) {
+      console.error(e);
+    }
   }
+
   // Reset Forggot password
-  ForgotPassword(passwordResetEmail: string) {
-    return this.afAuth
-      .sendPasswordResetEmail(passwordResetEmail)
-      .then(() => {
-        window.alert('Password reset email sent, check your inbox.');
-      })
-      .catch((error) => {
-        window.alert(error);
-      });
+  async ForgotPassword(passwordResetEmail: string) {
+    try {
+      await this.afAuth.sendPasswordResetEmail(passwordResetEmail);
+      window.alert('Password reset email sent, check your inbox.');
+    } catch (e) {
+      console.error(e);
+    }
   }
   // Returns true when user is looged in and email is verified
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem('user')!);
     return user !== null && user.emailVerified !== false ? true : false;
   }
+
   // Sign in with Google
-  GoogleAuth() {
-    return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
-      if (res) {
-        this.router.navigate(['dashboard']);
-      }
-    });
+  async GoogleAuth() {
+    try {
+      await this.AuthLogin(new auth.GoogleAuthProvider());
+      this.router.navigate(['dashboard']);
+    } catch (e) {
+      console.error(e);
+    }
   }
   // Auth logic to run auth providers
-  AuthLogin(provider: any) {
-    return this.afAuth
-      .signInWithPopup(provider)
-      .then((result) => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
-        this.SetUserData(result.user);
-      })
-      .catch((error) => {
-        window.alert(error);
-      });
+  async AuthLogin(provider: any) {
+    try {
+      let result = await this.afAuth.signInWithPopup(provider);
+      localStorage.setItem('user', JSON.stringify(result.user));
+      this.SetUserData(result.user);
+    } catch (e) {
+      console.error(e);
+    }
   }
   /* Setting up user data when sign in with username/password,
   sign up with username/password and sign in with social auth
@@ -126,6 +135,7 @@ export class AuthService {
     const userRef: AngularFirestoreDocument<any> = this.afs.doc(
       `users/${user.uid}`
     );
+
     const userData: User = {
       uid: user.uid,
       email: user.email,
@@ -133,15 +143,20 @@ export class AuthService {
       photoURL: user.photoURL,
       emailVerified: user.emailVerified,
     };
+
     return userRef.set(userData, {
       merge: true,
     });
   }
+
   // Sign out
-  SignOut() {
-    return this.afAuth.signOut().then(() => {
+  async SignOut() {
+    try {
+      await this.afAuth.signOut();
       localStorage.removeItem('user');
       this.router.navigate(['login']);
-    });
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
