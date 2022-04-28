13e7f3c964ae34de8f908245b933b1b103ee49a3
RussellABH <75578919+RussellABH@users.noreply.github.com>
2022-04-27 20:38:48 -0400

    Added goal updating from workout changes

--- backend/routes/workout-routes.js
+++ backend/routes/workout-routes.js
@@ -1,4 +1,5 @@
 const Workout = require("../models/workout");
+const Goal = require("../models/goal");
 const express = require("express");
 const exercise = require("../models/exercise");
 const router = express.Router();
@@ -9,8 +10,6 @@ router.get("/", async (req, res) => {
   try {
     // Need to put in firebase auth stuff
     const data = await Workout.find({
-      uid: req.user.user_id
-      // stuff with uid from firebase here
       uid: req.user.user_id
     });
     res.json(data);
@@ -28,13 +27,63 @@ router.get("/:name", async (req, res) => {
       // stuff with uid from firebase here
       uid: req.user.user_id,
       name: req.params.name,
-      uid: req.user.user_id
     });
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.put('/addSet', async (req, res) => {
+  try {
+    const { update, exercise_name, name } = req.body;
+
+    let data = await Workout.findOne({
+      uid: req.user.user_id,
+      name: name
+      // no auth stuff yet
+    });
+
+    data.exercises.forEach(async (exercise) => {
+      if (exercise.name == exercise_name) {
+        exercise.sets.push({ pounds: update.pounds, reps: update.reps })  // or maybe update[0] depending on what update turns out to be
+
+        // Update the goal for this set if it exists and current weight > this weight
+        try {
+          const goalData = await Goal.findOne({
+            uid: req.user.user_id,
+            exercise: exercise_name,
+          });
+
+          if (goalData && update.pounds > goalData.current) {
+            // Update the goal
+            await Goal.updateOne({
+              uid: req.user.user_id,
+              exercise: exercise_name,
+            }, {
+              current: update.pounds,
+            });
+          }
+
+        } catch (error) {
+          console.log("Could not check goal while updating an exercise: " + error.message);
+          res.status(500).json({ message: error.message });
+        }
+      }
+    });
+
+    await Workout.findOneAndUpdate({
+      // uid: req.user.user_id,
+      name: name,
+    }, data);
+
+    res.status(200).json({ message: "exercise updated"});
+  } catch (error) {
+    console.log("Error updating a exercise: " + error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 //update workout name
 router.put("/:name", async (req, res) => {
   name1 = "Failed_name"
@@ -48,14 +97,14 @@ router.put("/:name", async (req, res) => {
     name: name1
   }, (err, doc, res_) => {
     if (err) return res.status(500).json({error: err.message});
-    res.status(200).json({success: true})
+    res.status(200).json({success1: true})
   })
 });
 
 //Create new workout with that name
 router.post('/:name', async (req, res) => {
   var exercise_pool = [];
-  if(req.params.name.includes("Back Workout")){
+  if (req.params.name.includes("Back Workout")) {
     exercise_pool = [
       {
         name: "Pull Ups",
@@ -83,7 +132,7 @@ router.post('/:name', async (req, res) => {
       }
     ]
   }
-  else if(req.params.name.includes("Chest Workout")){
+  else if (req.params.name.includes("Chest Workout")) {
     exercise_pool = [
       {
         name: "Bench Press",
@@ -99,7 +148,7 @@ router.post('/:name', async (req, res) => {
       },
     ]
   }
-  else if(req.params.name.includes("Leg Workout")){
+  else if (req.params.name.includes("Leg Workout")) {
     exercise_pool = [
       {
         name: "Deadlift",
@@ -118,24 +167,24 @@ router.post('/:name', async (req, res) => {
         set: []
       },
       {
-        name:"Overhead Lunge",
+        name: "Overhead Lunge",
         set: []
       },
       {
-        name:"Weighted Step Up",
+        name: "Weighted Step Up",
         set: []
       },
       {
-        name:"Clean and Jerk",
+        name: "Clean and Jerk",
         set: []
       },
     ]
   }
   const date = new Date();
   // Data about date
-  var dd =date.getDate();
-  var mm= date.getMonth() + 1;
-  var yyyy= date.getFullYear();
+  var dd = date.getDate();
+  var mm = date.getMonth() + 1;
+  var yyyy = date.getFullYear();
   var year = `${yyyy}`.substring(2);
   var date_obj = `${mm}/${dd}/${year}`;
   try {
@@ -143,7 +192,7 @@ router.post('/:name', async (req, res) => {
       uid: req.user.user_id,
       name: req.params.name,
       exercises: exercise_pool,
-      date:date_obj
+      date: date_obj
     });
     console.log("Made a new workout for " + req.user.user_id);
     res.status(201).json({ message: "Workout created" });
@@ -152,58 +201,6 @@ router.post('/:name', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
-
-router.put('/addSet', async (req, res) => {
-  try {
-
-    const { update } = req.body;
-    const { exercise_name } = req.body;
-    // console.log(update)
-    // console.log(exercise_name)
-    const { name } = req.body;
-    // console.log(name)
-    // const { exercise_name } = req.params.exercise_name;
-    // res.status(200).json(req.body);
-
-    let data = await Workout.findOne({
-      uid: req.user.user_id,
-      name: name
-      // no auth stuff yet
-    })
-    // console.log(data)
-    let temp = data.exercises;
-    let count = 0;
-    data.exercises.forEach(exercise => {
-      
-      if (exercise.name == exercise_name) {
-        // console.log("updating")
-        // console.log(temp[count]["sets"])
-        temp[count]["sets"].push({pounds: update.pounds, reps: update.reps})  // or maybe update[0] depending on what update turns out to be
-        // console.log("after")
-        // console.log(temp[count]["sets"])
-        return;
-      }
-      count++;
-    })
-    
-
-    await Workout.findOneAndUpdate({
-      // uid: req.user.user_id,
-      name: name,
-    }, {
-        exercises: temp,
-    });
-    
-    // console.log(`updated exercise ${exercise_name} for ${req.user.name}`)
-    // console.log("Updated a exercise for " + req.user.name);
-    res.status(200).json({ message: "exercise updated", exercise: temp });
-  } catch (error) {
-    console.log("Error updating a exercise: " + error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
 
 router.put('/deleteSet', async (req, res) => {
   try {
@@ -223,10 +220,12 @@ router.put('/deleteSet', async (req, res) => {
       // no auth stuff yet
     });
 
+    console.log(data);
+
     let temp = data.exercises;
     let count = 0;
     data.exercises.forEach(exercise => {
-      
+
       if (exercise.name == exercise_name) {
         temp[count]["sets"] = update;  // or maybe update[0] depending on what update turns out to be
         return;
@@ -239,7 +238,7 @@ router.put('/deleteSet', async (req, res) => {
       uid: req.user.user_id,
       name: name,
     }, {
-        exercises: temp,
+      exercises: temp,
     });
     console.log(`updated exercise ${exercise_name} for ${req.user.name} (delete)`)
     // console.log("Updated a exercise for " + req.user.name);
@@ -252,7 +251,7 @@ router.put('/deleteSet', async (req, res) => {
 
 
 router.delete('/deleteExercise', async (req, res) => {
-  const name  = req.query.name;
+  const name = req.query.name;
   const exercise = req.query.exercise;
   let data = await Workout.findOne({
     uid: req.user.user_id,
@@ -277,9 +276,9 @@ router.delete('/deleteExercise', async (req, res) => {
   }, {
     exercises: temp
   }, (err, doc, res_) => {
-    if (err) return res.status(500).json({error: err.message});
+    if (err) return res.status(500).json({ error: err.message });
     console.log(`deleted exercise ${exercise} from workout ${name}`);
-    res.status(200).json({success: true})
+    res.status(200).json({ success: true })
   })
 
 
@@ -312,9 +311,9 @@ router.put("/addExercise", async (req, res) => {
   }, {
     exercises: temp
   }, (err, doc, res_) => {
-    if (err) return res.status(500).json({error: err.message});
+    if (err) return res.status(500).json({ error: err.message });
     console.log(`added exercise ${exercise_name} to workout ${name}`);
-    res.status(200).json({success: true})
+    res.status(200).json({ success: true })
   })
 
 });
